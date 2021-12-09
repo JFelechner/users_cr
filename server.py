b208a5e62539124f5e567aeb73808efc20032357
John Felechner <jfelechner@comcast.net>
2021-12-09 14:36:41 -0600

    CRUD Updates

--- server.py
+++ server.py
@@ -1,38 +1,81 @@
 
 
 from flask import Flask, render_template, request, redirect
-# import the class from friend.py
 from user import User
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    # call the get all classmethod to get all friends
     users = User.get_all()
     print(users)
     return render_template("index.html", users=users)
+
+
+
 
 
 @app.route('/create_user_page')
 def create_user_page():
     return render_template('create_user.html')
 
-
-# relevant code snippet from server.py
 @app.route('/create_user', methods=["POST"])
 def create_user():
-    # First we make a data dictionary from our request.form coming from our template.
-    # The keys in data need to line up exactly with the variables in our query string.
+
     data = {
         "fname": request.form["fname"],
         "lname" : request.form["lname"],
         "email" : request.form["email"]
     }
-    # We pass the data dictionary into the save method from the Friend class.
-    User.save(data)
-    # Don't forget to redirect after saving to the database.
+    user = User.save(data)
+    print(user)
+    return redirect(f"/show/{user}")
+
+
+
+
+
+@app.route('/show/<int:id>')
+def show_user(id):
+
+    data={
+        "id":id
+    }
+    users = User.get_one(data)
+    print(users)
+    return render_template('show.html', users=users)
+
+
+
+
+
+
+@app.route('/edit/<int:id>')
+def edit_user(id):
+
+    data={
+        "id":id
+    }
+    users = User.get_one(data)
+    print(users)
+    return render_template('edit.html', users=users)
+
+@app.route('/update/user', methods=["POST"])
+def update_user():
+
+    User.update_user(request.form)
     return redirect('/')
+
+
+
+
+
+@app.route('/delete/user', methods=["POST"])
+def delete_user():
+
+    User.delete(request.form)
+    return redirect('/')
+
 
 
 if __name__ == "__main__":
--- user.py
+++ user.py
@@ -1,6 +1,5 @@
-# import the function that will return an instance of a connection
+
 from mysqlconnection import connectToMySQL
-# model the class after the friend table from our database
 
 
 class User:
@@ -11,25 +10,36 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('users_shema').query_db(query)
-        # Create an empty list to append our instances of friends
         users = []
-        # Iterate over the db results and create instances of friends with cls.
         for user in results:
             users.append(cls(user))
         return users
 
-# import the function that will return an instance of a connection
-# model the class after the friend table from our database
+    @classmethod
+    def get_one(cls, data):
+        print("************************")
 
-    # ... other class methods
-    # class method to save our friend to the database
+        query  = "SELECT * FROM users WHERE users.id = %(id)s";
+        return connectToMySQL('users_shema').query_db(query,data)
+        
+
+    @classmethod
+    def update_user(cls, data):
+        query = "UPDATE users SET first_name = %(fname)s , last_name = %(lname)s , email = %(email)s, updated_at = NOW() WHERE id = %(id)s"
+        return connectToMySQL('users_shema').query_db(query, data)
+
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s"
+        return connectToMySQL('users_shema').query_db(query, data)
+
+
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
